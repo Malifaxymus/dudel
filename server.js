1cbd65cbff5db9fe53fd76b3a4f59bcc81cd6762
Brock Ross <m.brock.ross@gmail.com>
2020-06-06 16:24:30 -0600

    serve react app + basic socket handling

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io");
+const path = require("path");
+const io = require("socket.io")(http);
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+io.on("connection", (socket) => {
+  console.log("some buddy connected.");
+
+  socket.on("disconnect", () => {
+    console.log("some buddy disconnected.");
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
